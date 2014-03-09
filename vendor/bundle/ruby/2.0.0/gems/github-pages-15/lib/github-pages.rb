74368449ab43364e9a76b1d89e42fc00b31440a3
mfenner <mfenner@plos.org>
2014-03-09 08:20:54 +0100

    vendor gems for faster deployment

---
+++ vendor/bundle/ruby/2.0.0/gems/github-pages-15/lib/github-pages.rb
@@ -0,0 +1,27 @@
+class GitHubPages
+  VERSION = 15
+
+  # Jekyll and related dependency versions as used by GitHub Pages.
+  # For more information see:
+  # https://help.github.com/articles/using-jekyll-with-pages
+  def self.gems
+    {
+      "jekyll"       => "1.4.3",
+      "kramdown"     => "1.3.1",
+      "liquid"       => "2.5.5",
+      "maruku"       => "0.7.0",
+      "rdiscount"    => "2.1.7",
+      "redcarpet"    => "2.3.0",
+      "RedCloth"     => "4.2.9"
+    }
+  end
+
+  # Versions used by GitHub Pages, including github-pages gem and ruby version
+  # Useful for programmatically querying for the current-running version
+  def self.versions
+    gems.merge({
+      "github-pages" => VERSION.to_s,
+      "ruby"         => RUBY_VERSION
+    })
+  end
+end
